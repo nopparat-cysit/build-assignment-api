1de4f7a6190e9ca95836cfb04b6caaf5343124bd
nopparat <nopparat.cysit@gmail.com>
2026-02-02 00:26:57 +0700

    Implement assignment creation endpoint and update database connection settings

--- server/app.mjs
+++ server/app.mjs
@@ -1,10 +1,44 @@
 import express from "express";
+import connectionPool from "./utils/db.mjs";
 
 const app = express();
 const port = 4001;
 
+app.use(express.json());
+
+
 app.get("/test", (req, res) => {
-  return res.json("Server API is working 🚀");
+  res.json({ message: "Server API is working 🚀" });
+});
+
+app.post("/assignments", async (req, res) => {
+  try {
+    const newAssignment = {
+      ...req.body,
+      created_at: new Date(),
+      updated_at: new Date(),
+      published_at: new Date()
+    };
+
+    await connectionPool.query(
+      `INSERT INTO assignments (title, content, category, created_at, updated_at, published_at) VALUES ($1, $2, $3, $4, $5, $6)`,
+      [
+        newAssignment.title,
+        newAssignment.content,
+        newAssignment.category,
+        newAssignment.created_at,
+        newAssignment.updated_at,
+        newAssignment.published_at,
+      ]
+    );
+
+    res.status(201).json({ message: "Created assignment successfully" });
+  } catch (error) {
+    return res.status(500).json({ 
+      message: "Server could not create assignment because database connection",
+      error: error.message 
+    });
+  }
 });
 
 app.listen(port, () => {
