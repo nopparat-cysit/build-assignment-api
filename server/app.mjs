ff1e95c0995795b0f427244e6767516dde307f4f
nopparat <nopparat.cysit@gmail.com>
2026-02-02 14:12:06 +0700

    feat: Add CRUD endpoints for assignments: create, read, update, and delete

--- server/app.mjs
+++ server/app.mjs
@@ -41,6 +41,111 @@ app.post("/assignments", async (req, res) => {
   }
 });
 
+app.get("/assignments", async (req, res) => {
+  try {
+    const result = await connectionPool.query('SELECT * FROM assignments')
+    res.status(201).json({ 
+      message: "Read assignment successfully",
+      data: result.rows
+     });
+  } catch (error) {
+    return res.status(500).json({ 
+      message: "Server could not create assignment because database connection",
+      error: error.message 
+    });
+  }
+});
+
+app.get("/assignments/:assignment_id", async (req, res) => {
+  try {
+    let param = req.params.assignment_id;
+    const result = await connectionPool.query(
+      `SELECT * FROM assignments WHERE assignment_id = $1`,
+      [param]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: `Server could not find a requested assignment ID: ${param}`,
+      });
+    }
+    res.status(201).json({
+      message: "Read assignment successfully",
+      data: result.rows
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server could not create assignment because database connection",
+      error: error.message
+    });
+  }
+});
+
+app.put("/assignments/:assignmentId", async (req, res) => {
+  try {
+    const assignmentId = req.params.assignmentId;
+    const newAssignment = {
+      ...req.body,
+      updated_at: new Date()
+    };
+    const result = await connectionPool.query(`
+      UPDATE assignments 
+      SET title = $2, 
+          content = $3,
+          category = $4,
+          updated_at = $5
+      WHERE assignment_id = $1
+      RETURNING *;
+    `, [
+      assignmentId,
+      newAssignment.title,
+      newAssignment.content,
+      newAssignment.category,
+      newAssignment.updated_at
+    ]);
+
+    if (result.rows.length === 0) {
+      // ดัก 404: assignment ไม่พบ
+      return res.status(404).json({
+        message: `Assignment with id ${assignmentId} not found.`
+      });
+    }
+
+    res.status(201).json({
+      message: "Update assignment successfully",
+      data: result.rows
+    });
+  } catch (error) {
+    return res.status(500).json({ 
+      message: "Server could not create assignment because database connection",
+      error: error.message 
+    });
+  }
+});
+
+app.delete("/assignments/:assignment_id", async (req, res) => {
+  try {
+    let param = req.params.assignment_id;
+    const result = await connectionPool.query(
+      `DELETE FROM assignments WHERE assignment_id = $1 RETURNING *`,
+      [param]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({
+        message: `Server could not find a requested assignment ID: ${param}`,
+      });
+    }
+    res.status(201).json({
+      message: "Delete assignment successfully",
+      data: result.rows
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Server could not create assignment because database connection",
+      error: error.message
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
 });
